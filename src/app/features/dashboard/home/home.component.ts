fce24e31c9b9bb4986348067a77028375f076389
Rafael E. Cortez <rafael.ecortez@gmail.com>
2024-07-29 23:56:41 -0300

    home modified

--- src/app/features/dashboard/home/home.component.ts
+++ src/app/features/dashboard/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {StudentsService} from "../../../core/services/students.service";
+import {CoursesService} from "../../../core/services/courses.service";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  courseQuantity: number = 0;
+  studentQuantity: number = 0;
 
+  constructor(private studentsService: StudentsService, private courseService: CoursesService) {
+    this.courseService.getCourses().subscribe({
+      next: courses => {this.courseQuantity = courses.length},
+    })
+
+    this.studentsService.getStudents().subscribe({
+      next: students => {this.studentQuantity = students.length},
+    })
+  }
 }
